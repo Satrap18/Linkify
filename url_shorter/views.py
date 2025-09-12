de22505504874321ea6fd979bb36a35a2ee2ecad
Satrap18 <kreza6517@gmail.com>
2025-09-12 16:28:00 +0330

    feat: add shortview(viewsets)

--- url_shorter/views.py
+++ url_shorter/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import viewsets
+from url_shorter.models import Shorter
+from url_shorter.serializers import ShorterSerializer
 # Create your views here.
+
+class ShorterView(viewsets.ModelViewSet):
+
+    queryset = Shorter.objects.all()
+    serializer_class = ShorterSerializer
