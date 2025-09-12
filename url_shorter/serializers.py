b9fd4810551324fe1368977d7b9058b124dfe014
Satrap18 <kreza6517@gmail.com>
2025-09-12 16:28:43 +0330

    feat: add serializers (shorter model)

---
+++ url_shorter/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from url_shorter.models import Shorter
+import hashlib, base64
+from url_shorter.models import Shorter
+
+class ShorterSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Shorter
+        fields = ['id', 'original_url', 'url']
+
+    
+    def validate_url(self, value):
+        
+        url = value
+        h = hashlib.sha256(url.encode('utf-8')).digest()
+        en = base64.urlsafe_b64encode(h)
+        short_link = en[:8]
+        return short_link.decode('utf-8').rstrip('=')
