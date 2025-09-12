ccc5e498f7b2fa9bf5743e6d32078ca5825189a0
Satrap18 <kreza6517@gmail.com>
2025-09-12 16:25:58 +0330

    feat: create model shorter

--- url_shorter/models.py
+++ url_shorter/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Shorter(models.Model):
+    original_url = models.URLField(max_length=128,blank=True)
+    url = models.URLField(max_length=128,unique=True,blank=True)
