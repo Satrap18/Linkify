c06e801a91433cb080dbb5a4518446c385fda7f1
Satrap18 <kreza6517@gmail.com>
2025-09-12 16:27:00 +0330

    feat: use DefaultRouter and router

--- url_shorter/urls.py
+++ url_shorter/urls.py
@@ -1,5 +1,11 @@
-from django.urls import path
+from django.urls import path, include
+from url_shorter.views import ShorterView
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'url', ShorterView, basename='url')
+
 
 urlpatterns = [
-    
+    path('', include(router.urls)),
 ]
