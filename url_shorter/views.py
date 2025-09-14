44701ed8780b3b9be97d781ad16356bfbf67c28d
Satrap18 <kreza6517@gmail.com>
2025-09-14 17:16:53 +0330

    chore: add view template

--- url_shorter/views.py
+++ url_shorter/views.py
@@ -4,9 +4,17 @@ from rest_framework import status
 from rest_framework import viewsets
 from url_shorter.models import Shorter
 from url_shorter.serializers import ShorterSerializer
+from django.views import View
 # Create your views here.
 
 class ShorterView(viewsets.ModelViewSet):
 
     queryset = Shorter.objects.all()
     serializer_class = ShorterSerializer
+
+
+class ReturnRequestView(View):
+
+    def get(self, request, *args, **kwargs):
+
+        return render(request, "main.html")
