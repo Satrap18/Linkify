51fc742ba9c2cdc38f3bd50c2aa045dc2b085b4b
Satrap18 <kreza6517@gmail.com>
2025-09-17 16:47:26 +0330

    feat: add return request view and redirect short url

--- url_shorter/views.py
+++ url_shorter/views.py
@@ -1,10 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
 from url_shorter.models import Shorter
 from url_shorter.serializers import ShorterSerializer
 from django.views import View
+import hashlib, base64
 # Create your views here.
 
 class ShorterView(viewsets.ModelViewSet):
@@ -18,3 +19,21 @@ class ReturnRequestView(View):
     def get(self, request, *args, **kwargs):
 
         return render(request, "main.html")
+
+    def post(self, request, *args, **kwargs):
+
+        original_url = request.POST.get("original_url")
+        url_hash = hashlib.sha256(original_url.encode('utf-8')).digest()
+        base64_encoded = base64.urlsafe_b64encode(url_hash)
+        short_code_bytes = base64_encoded[:8]
+        short_code = short_code_bytes.decode('utf-8').rstrip('=')
+
+        Shorter.objects.create(original_url=original_url, url=short_code)
+        return render(request, 'main.html', {'short_code':f'http://localhost:8000/{short_code}',"original_url": ""})
+
+class RedirectShortUrl(View):
+
+    def get(self, request, short_url, *args, **kwargs):
+
+        shorter_instance = get_object_or_404(Shorter, url=short_url)
+        return redirect(shorter_instance.original_url)
