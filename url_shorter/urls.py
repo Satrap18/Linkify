b471ca2169ce658275117085eec1829e289595f3
Satrap18 <kreza6517@gmail.com>
2025-09-17 16:46:36 +0330

    feat: add short_url link

--- url_shorter/urls.py
+++ url_shorter/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from url_shorter.views import ShorterView, ReturnRequestView
+from url_shorter.views import ShorterView, ReturnRequestView, RedirectShortUrl
 from rest_framework.routers import DefaultRouter
+
 
 router = DefaultRouter()
 router.register(r'url', ShorterView, basename='url')
@@ -8,5 +9,6 @@ router.register(r'url', ShorterView, basename='url')
 
 urlpatterns = [
     path('api/', include(router.urls)),
-    path('', ReturnRequestView.as_view(), name='main')
+    path('', ReturnRequestView.as_view(), name='main'),
+    path('<str:short_url>/', RedirectShortUrl.as_view(), name='redirect_short_url'),
 ]
