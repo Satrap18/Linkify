531dad8b549d372addb0c2dd04fe02ebf297c321
Satrap18 <kreza6517@gmail.com>
2025-09-17 17:12:17 +0330

    fix bug

--- url_shorter/views.py
+++ url_shorter/views.py
@@ -20,26 +20,23 @@ class ReturnRequestView(View):
 
         return render(request, "main.html")
 
-class ReturnRequestView(View):
-
     def post(self, request, *args, **kwargs):
-        original_url = request.POST.get("original_url")
-        url_hash = hashlib.sha256(original_url.encode('utf-8')).digest()
-        base64_encoded = base64.urlsafe_b64encode(url_hash)
-        short_code_bytes = base64_encoded[:8]
-        short_code = short_code_bytes.decode('utf-8').rstrip('=')
+            original_url = request.POST.get("original_url")
+            url_hash = hashlib.sha256(original_url.encode('utf-8')).digest()
+            base64_encoded = base64.urlsafe_b64encode(url_hash)
+            short_code_bytes = base64_encoded[:8]
+            short_code = short_code_bytes.decode('utf-8').rstrip('=')
 
-        shorter, created = Shorter.objects.get_or_create(
-            original_url=original_url,
-            defaults={'url': short_code}
-        )
+            shorter, created = Shorter.objects.get_or_create(
+                original_url=original_url,
+                defaults={'url': short_code}
+            )
 
-        return render(
-            request, 
-            'main.html', 
-            {'short_code': f'http://localhost:8000/{shorter.url}', "original_url": ""}
-        )
-
+            return render(
+                request, 
+                'main.html', 
+                {'short_code': f'http://localhost:8000/{shorter.url}', "original_url": ""}
+            )
 
 class RedirectShortUrl(View):
 
