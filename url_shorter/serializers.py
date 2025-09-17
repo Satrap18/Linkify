9cebc89b931e55bac899f4becaeb6422544fe8cd
Satrap18 <kreza6517@gmail.com>
2025-09-17 16:45:37 +0330

    fix: fix validate url

--- url_shorter/serializers.py
+++ url_shorter/serializers.py
@@ -11,8 +11,9 @@ class ShorterSerializer(serializers.ModelSerializer):
 
     
     def validate_url(self, value):
-        url = self.initial_data['original_url']
-        h = hashlib.sha256(url.encode('utf-8')).digest()
-        en = base64.urlsafe_b64encode(h)
-        short_link = en[:8]
-        return short_link.decode('utf-8').rstrip('=')
+        
+        original_url = self.initial_data['original_url']
+        url_hash = hashlib.sha256(original_url.encode('utf-8')).digest()
+        base64_encoded = base64.urlsafe_b64encode(url_hash)
+        short_code_bytes = base64_encoded[:8]
+        return short_code_bytes.decode('utf-8').rstrip('=')
