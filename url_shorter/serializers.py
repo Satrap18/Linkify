8c5294b005242d3c701934d91e1345a192bde7ab
Satrap18 <kreza6517@gmail.com>
2025-09-17 17:09:58 +0330

    fix: fix bug

--- url_shorter/serializers.py
+++ url_shorter/serializers.py
@@ -4,16 +4,16 @@ import hashlib, base64
 from url_shorter.models import Shorter
 
 class ShorterSerializer(serializers.ModelSerializer):
-    
     class Meta:
         model = Shorter
         fields = ['id', 'original_url', 'url']
+        read_only_fields = ['url']
 
-    
-    def validate_url(self, value):
-        
-        original_url = self.initial_data['original_url']
+    def create(self, validated_data):
+        original_url = validated_data['original_url']
         url_hash = hashlib.sha256(original_url.encode('utf-8')).digest()
         base64_encoded = base64.urlsafe_b64encode(url_hash)
         short_code_bytes = base64_encoded[:8]
-        return short_code_bytes.decode('utf-8').rstrip('=')
+        short_code = short_code_bytes.decode('utf-8').rstrip('=')
+        validated_data['url'] = short_code
+        return super().create(validated_data)
