6fdf463c0ce7e99e1aebcf0012cd443434d4a311
Satrap18 <kreza6517@gmail.com>
2025-09-25 23:59:00 +0330

    chore: update

--- url_shorter/models.py
+++ url_shorter/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Shorter(models.Model):
     original_url = models.URLField(max_length=128,blank=True)
     url = models.URLField(max_length=128,unique=True,blank=True)
+
+    def __str__(self):
+        return self.url
