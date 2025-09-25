bd8b0c35b7999f64a0c0772679d8616281f41eb2
Satrap18 <kreza6517@gmail.com>
2025-09-25 23:40:52 +0330

    test: add unit test for Shorter model original_url field

--- url_shorter/tests.py
+++ url_shorter/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-
+from url_shorter.models import Shorter
 # Create your tests here.
+class ShorterTestCase(TestCase):
+
+    def setUp(self):
+        print("setUp: Run once for every test method to set up clean data.")
+        Shorter.objects.create(original_url='https://github.com/satrap18')
+
+    
+    def test_url_original(self):
+        original_urls = Shorter.objects.get(id=1)
+        self.assertEqual(original_urls.original_url, 'https://github.com/satrap18')
