6942960081208b829fb6f1d08889ed3f34dba02c
Satrap18 <kreza6517@gmail.com>
2025-09-26 23:59:00 +0330

    Add test to verify url field value

--- url_shorter/tests.py
+++ url_shorter/tests.py
@@ -1,13 +1,16 @@
 from django.test import TestCase
 from url_shorter.models import Shorter
+from django.db import IntegrityError
 # Create your tests here.
 class ShorterTestCase(TestCase):
 
     def setUp(self):
-        print("setUp: Run once for every test method to set up clean data.")
-        Shorter.objects.create(original_url='https://github.com/satrap18')
+        Shorter.objects.create(original_url='https://github.com/satrap18', url='http://short.ly/abc123')
 
-    
     def test_url_original(self):
-        original_urls = Shorter.objects.get(id=1)
-        self.assertEqual(original_urls.original_url, 'https://github.com/satrap18')
+        shorter = Shorter.objects.get(id=1)
+        self.assertEqual(shorter.original_url, 'https://github.com/satrap18')
+
+    def test_url_field(self):
+        shorter = Shorter.objects.get(id=1)
+        self.assertEqual(shorter.url, 'http://short.ly/abc123')
